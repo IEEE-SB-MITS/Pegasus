d63e0165a8985963b714dea581804ac7fdae3e5b
Rohit <heyrg7@gmail.com>
2023-10-10 19:26:09 +0530

    added comming soon initial'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Optin from './components/optin/optin';
+import Preloader from './components/preloader/preloader';
+import './components/Countdown/Timer'
+import CountDown from './components/Countdown/Timer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>
+          Website
+          <br />
+          Coming Soon
+        </h1>
+        <CountDown/>
+        <Optin/>
+        <Preloader/>
+      </div>
     </div>
   );
 }
