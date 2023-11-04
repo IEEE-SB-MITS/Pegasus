78d2c528b1e298de628dc7cdd0eecaf6bc8f7ccb
rohit george <code.mastermind.07@gmail.com>
2023-11-04 19:00:47 +0530

    updated timeline sec

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import "./App.css";
 import "./index.css";
-import Hack from "./pages/hack";
 import HackNav from "./common/HackNav";
 import Expo from "./pages/expo/expo";
 
 import {Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    // <div className="App h-[95vh] md:h-screen lg:h-screen">
-    //   <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-    //     <div className={`${styles.boxWidth}`}>
-    //       <Navbar />
-    //     </div>
-    //   </div>
-
-    //   <div className={`bg-primary ${styles.flexStart}`}>
-    //     <div className={`${styles.boxWidth}`}>
-    //       <Homepage />
-    //     </div>
-    //   </div>
-    // </div>
     <>
        <div className="App ">
       <Routes>
-        {/* <Route path="/" element={<Hero />} /> */}
-        <Route path="/hack" element={<Hack />} />
         <Route path="/" element={<Expo />} />
       
       </Routes>
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,4 @@
-import cloud_left from "./cloud-left.png"
-import cloud_right from "./cloud-right.png"
+
 import CSlogo from "./CS white.png"
 import ieeelogo from "./ieee-logo.png"
 import pegasus from "./newpega.png"
@@ -13,7 +12,6 @@ import ball from "./ballDesign.svg"
 import bkgnd from "./background.svg"
 import aboutusImg from "./aboutUs.svg"
 import chair from "./akshay.jpg"
-import person from "./person.jpg"
 import call from "./call.svg"
 import mail from "./mail.svg"
 import viceChair from "./vicechair.jpg"
@@ -22,8 +20,6 @@ import joana from "./joana.jpg"
 import sideimage from "./long-side.svg"
 import sideball from "./sideimage.svg"
 export {
-    cloud_left,
-    cloud_right,
     CSlogo,
     ieeelogo,
     pegasus,
@@ -36,7 +32,6 @@ export {
     ball,
     bkgnd,
     aboutusImg,
-    person,
     chair,
     call,
     mail,
