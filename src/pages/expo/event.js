32e2f109217d884ce84251d06203ae3738ca27b0
par-zival <21cs091@mgits.ac.in>
2023-11-08 17:31:11 +0530

    event 3

--- src/pages/expo/event.js
+++ src/pages/expo/event.js
@@ -37,7 +37,7 @@ export const Event = () => {
                                 
       </div>
       <a
-                href="#WS"
+                href="https://docs.google.com/forms/d/e/1FAIpQLSeWPmXPSkK-Zak3KVP6fh3Gex1nbjkTZhmFvbMBoUUA3Ke3sw/viewform"
                 id="evntreg"
                 className="m-6"
               > <button class="relative w-[10rem] py-2 px-8 text-black text-base font-bold uppercase rounded-[50px] overflow-hidden bg-white transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-[50px] hover:before:left-0">
