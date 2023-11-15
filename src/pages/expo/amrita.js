a4a7b7c55c81aa2495b8a2005445ef9ae7b0db38
par-zival <21cs091@mgits.ac.in>
2023-11-15 11:31:16 +0530

    new16

--- src/pages/expo/amrita.js
+++ src/pages/expo/amrita.js
@@ -12,11 +12,13 @@ export const Event = () => {
  <div className="group before:hover:scale-95 before:hover:h-[25rem] before:hover:w-[20rem] before:hover:h-70 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-44 before:rounded-t-2xl before:bg-gradient-to-bl from-indigo-300 via-red-400 to-indigo-700 before:absolute before:top-0 md:w-[20rem] w-[10rem  ] md:h-[25rem] h-[15rem] relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden">
   <img className="w-28 h-28 bg-red-900 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-150 group-hover:-translate-x-24  group-hover:-translate-y-20 transition-all duration-500" src={Events} alt='event'/>
   <div className="z-10  group-hover:-translate-y-10 transition-all  duration-500">   <span className="md:text-3xl text-indigo-900  text-[13px] font-bold">Amrita Jiju</span>
-    <p className='font-semibold text-purple-900 md:text-[16px]  text-[9px]'> Introduction to Tinkercad</p>
-    <p></p>
+    <p className='font-semibold text-purple-900 md:text-[16px]  text-[9px]'> Introduction to Tinkercad<br/></p>
+  <p className='font-semibold text-purple-900 md:text-[16px]  text-[9px]'>    18th November 2023</p>
+    
   </div>
   <a href="https://bit.ly/Introduction_to_Tinkercad" class="relative w-[10rem] text-purple-900 py-2 px-8 text-black text-base font-bold uppercase rounded-[50px] overflow-hidden bg-white transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-[50px] hover:before:left-0 hover:text-red-900" >REGISTER</a>
 </div>
+
   </div>
   
     
