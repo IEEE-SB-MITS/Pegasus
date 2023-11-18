0552e3a28a3ce0d95e40c946ed9b3ec550fd4953
rohit george <code.mastermind.07@gmail.com>
2023-11-18 09:49:53 +0530

    added preloader

--- src/App.js
+++ src/App.js
@@ -3,19 +3,37 @@ import "./index.css";
 import HackNav from "./common/HackNav";
 import Expo from "./pages/expo/expo";
 
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react"; // Import useState here
+// import Loading from "./constants/Loading";
+import Preloader from "./constants/preloader";
 
-import {Routes,Route} from 'react-router-dom'
 function App() {
+  const [isLoading, setIsLoading] = useState(true); // Declare useState inside the component
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      // Page has finished loading
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <>
-       <div className="App ">
-      <Routes>
-        <Route path="/" element={<Expo />} />
-      
-      </Routes>
-    </div>
-    <HackNav/>
-  
+      <div>
+        {isLoading ? (
+          <Preloader/>
+        ) : (
+          <>
+            <div className="App ">
+              <Routes>
+                <Route path="/" element={<Expo />} />
+              </Routes>
+            </div>
+            <HackNav />
+          </>
+        )}
+      </div>
     </>
   );
 }
---
+++ src/constants/preloader.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import './preloader.css'
+import { PreLoaderAnim } from "./animations";
+
+const Preloader = () => {
+    useEffect(()=>{
+        PreLoaderAnim()
+    }, []);
+
+    return (
+        <div className="preloader">
+            <div className="texts-container">
+                <span>Innovate,</span>
+                <span>Create,</span>
+                <span>Improve.</span>
+            </div>
+        </div>
+    );
+};
+
+export default Preloader;
