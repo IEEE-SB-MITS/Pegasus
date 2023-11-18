23c0ea856af2a4000aeb76f21d59c14b21a4d0a5
rohit george <code.mastermind.07@gmail.com>
2023-11-18 10:08:23 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -4,36 +4,43 @@ import HackNav from "./common/HackNav";
 import Expo from "./pages/expo/expo";
 
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react"; // Import useState here
+// import { useEffect, useState } from "react"; // Import useState here
 // import Loading from "./constants/Loading";
 import Preloader from "./constants/preloader";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true); // Declare useState inside the component
+  // const [isLoading, setIsLoading] = useState(true); // Declare useState inside the component
 
-  useEffect(() => {
-    window.addEventListener("load", () => {
-      // Page has finished loading
-      setIsLoading(false);
-    });
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener("load", () => {
+  //     // Page has finished loading
+  //     setIsLoading(false);
+  //   });
+  // }, []);
 
   return (
     <>
-      <div>
+      {/* <div>
         {isLoading ? (
           <Preloader/>
-        ) : (
-          <>
+          ) : (
+            <>
             <div className="App ">
-              <Routes>
-                <Route path="/" element={<Expo />} />
-              </Routes>
+            <Routes>
+            <Route path="/" element={<Expo />} />
+            </Routes>
             </div>
             <HackNav />
-          </>
-        )}
+            </>
+            )}
+          </div> */}
+      <Preloader />
+      <div className="App ">
+        <Routes>
+          <Route path="/" element={<Expo />} />
+        </Routes>
       </div>
+      <HackNav />
     </>
   );
 }
